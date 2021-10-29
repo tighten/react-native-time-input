35b0beafb8596d1f811656ae03277b68ef3f8eee
TammyTee <TammyTee@users.noreply.github.com>
2021-10-29 15:00:02 -0400

    Add hooks, utils, and dev dependencies for time input component

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useCallback, useState } from 'react';
+import _ from 'lodash';
+
+export default (obj = null, wait = 1000) => {
+    const [state, setState] = useState(obj);
+
+    const setDebouncedState = (value: any) => {
+        debounce(value);
+    };
+
+    const debounce = useCallback(
+        _.debounce((prop) => {
+            setState(prop);
+        }, wait),
+        []
+    );
+
+    return [state, setDebouncedState];
+};
---
+++ src/utils/theme.ts
@@ -0,0 +1,9 @@
+export default {
+  black: '#2e2e2e',
+  blue: '#2980b9',
+  grey: '#a6a6a6',
+  lightGrey: '#d7d7d7',
+  offWhite: '#e6e6e6',
+  red: '#c0392b',
+  white: '#ffffff',
+}
