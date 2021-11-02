1402ca9ae95d3147d0a460ba21b815c161e9cc7a
TammyTee <TammyTee@users.noreply.github.com>
2021-11-02 15:49:11 -0400

    Handle onTimeChange callback

--- src/index.tsx
+++ src/index.tsx
@@ -23,6 +23,7 @@ export default function TimeInput (
     theme = defaultTheme,
   }: TimeInputProps
 ): JSX.Element | null {
+  const initialRender = useRef(true);
   const animation = useRef(new Animated.Value(0)).current;
   const [componentReady, setComponentReady] = useState<boolean>(false);
   const [componentTheme, setComponentTheme] = useState<TimeInputTheme | null>(null);
@@ -47,7 +48,6 @@ export default function TimeInput (
   const [validTime, setValidTime] = useState<boolean>(true);
 
   const handleTimeValueReady = (isValid: boolean, updated: string): void => {
-    // TODO: call onTimeChange here?
     setTime(updated);
     setValidTime(isValid);
   }
@@ -81,6 +81,15 @@ export default function TimeInput (
     handleMeridiemChange();
     return () => animation.stopAnimation();
   }, [meridiem, setMeridiem]);
+
+  useEffect(() => {
+    if (initialRender.current) {
+      initialRender.current = false;
+      return;
+    }
+
+    onTimeChange(`${time} ${meridiem}`, validTime);
+  }, [time, meridiem, validTime, onTimeChange])
 
   if (!componentReady || !componentStyle || !componentTheme) return null;
 
